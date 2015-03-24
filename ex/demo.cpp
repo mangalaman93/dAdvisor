5376b220b77e35848142d3ecaf2ce6a955a7cc55
Aman Mangal <mangalaman93@gmail.com>
2015-03-23 23:49:59 -0400

    added network monitoring

--- ex/demo.cpp
+++ ex/demo.cpp
@@ -5,14 +5,37 @@ using namespace std;
 class SimpleMonitor : public Monitor {
 private:
   void checkAndSet(Guest *guest) {
+    // CPU allocation
     float usage = guest->getCpuUsage();
     float allocation = guest->getHardCpuShares();
     cout<<"usage "<<usage<<", allocation "<<allocation<<" for guest ";
     cout<<guest->getName()<<endl;
 
     if(allocation*SAFETY_FACTOR < usage) {
-      cout<<"=> usage is about to cross allocation for guest"<<endl;
+      cout<<"=> usage is about to cross"<<endl;
       guest->setHardCpuShares(allocation*(1+INCREMENT_FACTOR));
+    }
+
+    // Network In allocation
+    usage = guest->getNetworkInUsage();
+    allocation = guest->getNetworkInAllocation();
+    cout<<"net in usage "<<usage<<", allocation "<<allocation<<" for guest ";
+    cout<<guest->getName()<<endl;
+
+    if(allocation*SAFETY_FACTOR < usage) {
+      cout<<"=> net in usage is about to cross"<<endl;
+      guest->setNetworkInBW(allocation*(1+INCREMENT_FACTOR));
+    }
+
+    // Network Our allocation
+    usage = guest->getNetworkOutUsage();
+    allocation = guest->getNetworkOutAllocation();
+    cout<<"net Out usage "<<usage<<", allocation "<<allocation<<" for guest ";
+    cout<<guest->getName()<<endl;
+
+    if(allocation*SAFETY_FACTOR < usage) {
+      cout<<"=> net Out usage is about to cross"<<endl;
+      guest->setNetworkOutBW(allocation*(1+INCREMENT_FACTOR));
     }
 
     return;
@@ -29,6 +52,8 @@ int main(int argc, char *argv[]) {
   } else {
     LProcess lp("voip", "eth0", "", atoi(argv[1]));
     lp.setHardCpuShares(10);
+    lp.setNetworkInBW(10);
+    lp.setNetworkOutBW(10);
     sleep(2);
 
     SimpleMonitor monitor;

2ac624d7f6e5dfa76c867d54b980482dc2fa8fb1
Aman Mangal <mangalaman93@gmail.com>
2015-03-31 21:35:24 -0400

    log.py taking process usage instead of interface usage

--- priv/log.py
+++ priv/log.py
@@ -4,16 +4,29 @@ import os
 import sys
 import tempfile
 import time
+import subprocess
 
 USAGE_CHECK_PERIOD = 1
-RX_BYTES = "/sys/class/net/eth0/statistics/rx_bytes"
-TX_BYTES = "/sys/class/net/eth0/statistics/tx_bytes"
+# RX_BYTES = "/sys/class/net/eth0/statistics/rx_bytes"
+# TX_BYTES = "/sys/class/net/eth0/statistics/tx_bytes"
+RX_BYTES = "/proc/<pid>/net/dev"
 
 def cpu(pid):
     return sum(map(float, file('/proc/%s/stat' % pid).read().split()[13:17]))
 
-def net(path):
-    return float(file(path).read())
+# def net(path):
+#     return float(file(path).read())
+
+def system_cmd(cmd):
+    p = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE)
+    # if only the command executed successfully
+    if p.wait() == 0:
+      return p.stdout.readlines()
+    else:
+      raise Exception("error!")
+
+def net(pid, col):
+    return int(system_cmd("cat /proc/"+str(pid)+"/net/dev | awk '/eth0/ {print $"+str(col)+"}'")[0])
 
 def main():
     if len(sys.argv) < 2 or len(sys.argv) > 2 or sys.argv[1] == '-h':
@@ -25,23 +38,27 @@ def main():
     rx_stats = []
     tx_stats = []
     cpu_prev = cpu(pid)
-    rx_prev = net(RX_BYTES)
-    tx_prev = net(TX_BYTES)
+    rx_prev = new = net(pid, 2)
+    tx_prev = new = net(pid, 10)
+    # rx_prev = net(RX_BYTES)
+    # tx_prev = net(TX_BYTES)
 
     try:
         while True:
             time.sleep(USAGE_CHECK_PERIOD)
             # CPU
             new = cpu(pid)
-            cpu_stats.append((new-cpu_prev)/USAGE_CHECK_PERIOD)
+            print ((new-cpu_prev)/USAGE_CHECK_PERIOD)
             cpu_prev = new
             # RX
-            new = net(RX_BYTES)
-            rx_stats.append((new-rx_prev)/USAGE_CHECK_PERIOD)
+            # new = net(RX_BYTES)
+            new = net(pid, 2)
+            print ((new-rx_prev)/USAGE_CHECK_PERIOD)
             rx_prev = new
             # TX
-            new = net(TX_BYTES)
-            tx_stats.append((new-tx_prev)/USAGE_CHECK_PERIOD)
+            # new = net(TX_BYTES)
+            new = net(pid, 10)
+            print ((new-tx_prev)/USAGE_CHECK_PERIOD)
             tx_prev = new
     except:
         pass

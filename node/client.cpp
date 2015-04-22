2ad787f06ee5c14438d5b2ab6d5fa3504ac89409
Aman Mangal <mangalaman93@gmail.com>
2015-04-22 01:30:05 -0400

    evpath getting compiled

--- node/client.cpp
+++ node/client.cpp
@@ -2,80 +2,67 @@
 
 static LProcess *lp;
 
-static int
-cmd_handler(CManager cm, void *vevent, void *client_data, attr_list attrs)
-{
-    alloc_info_ptr event = (alloc_info_ptr)vevent;
-    // printf("%2f\t%2f\t%2f\n",
-    //        event->cpu_alloc, event->network_in_alloc, event->network_out_alloc);
-    printf("%2f\n", event->network_out_alloc);
+static int cmd_handler(CManager cm, void *vevent, void *client_data,
+    attr_list attrs) {
+  alloc_info_ptr event = (alloc_info_ptr)vevent;
+  printf("%2f\t%2f\n", event->cpu_alloc, event->network_out_alloc);
 
-    // if(event->cpu_alloc > 0) {
-    //     lp->setHardCPUShares(event->cpu_alloc);
-    // }
+  // if(event->cpu_alloc > 0) {
+  //     lp->setHardCPUShares(event->cpu_alloc);
+  // }
 
-    // if(event->network_in_alloc > 0) {
-    //     lp->setNetworkInBW(event->network_in_alloc);
-    // }
+  // if(event->network_out_alloc > 0) {
+  //   lp->setNetworkOutBW(event->network_out_alloc);
+  // }
 
-    if(event->network_out_alloc > 0) {
-        lp->setNetworkOutBW(event->network_out_alloc);
-    }
-
-    return 1;
+  return 1;
 }
 
-/* this file is evpath/examples/net_send.c */
-int main(int argc, char **argv)
-{
-    CManager cm = CManager_create();
-    CMlisten(cm);
+int main(int argc, char **argv) {
+  if(argc < 3) {
+    printf("usage: %s <master contact list> <pid>\n", argv[0]);
+  }
 
-    // parsing the master contact list
-    char string_list[2048];
-    EVstone remote_stone;
-    if(sscanf(argv[1], "%d:%s", &remote_stone, &string_list[0]) != 2) {
-	   printf("Bad arguments \"%s\"\n", argv[1]);
-	   exit(0);
-    }
+  CManager cm = CManager_create();
+  CMlisten(cm);
 
-    // send stone
-    EVstone sstone = EValloc_stone(cm);
-    attr_list contact_list = attr_list_from_string(string_list);
-    EVassoc_bridge_action(cm, sstone, contact_list, remote_stone);
+  // parsing the master contact list
+  char string_list[2048];
+  EVstone remote_stone;
+  if(sscanf(argv[1], "%d:%s", &remote_stone, &string_list[0]) != 2) {
+   printf("Bad arguments \"%s\"\n", argv[1]);
+   exit(0);
+  }
 
-    // receive command stone
-    EVstone rstone = EValloc_stone(cm);
-    EVassoc_terminal_action(cm, rstone, command_list, cmd_handler, NULL);
+  // send stone
+  EVstone sstone = EValloc_stone(cm);
+  attr_list contact_list = attr_list_from_string(string_list);
+  EVassoc_bridge_action(cm, sstone, contact_list, remote_stone);
 
-    // sending allocation
-    EVsource source = EVcreate_submit_handle(cm, sstone, data_list);
-    lp = new LProcess("client", atoi(argv[2]), "eth0");
-    // lp->setHardCPUShares(100);
-    // lp->setNetworkInBW(1000);
-    lp->setNetworkOutBW(1000);
+  // receive command stone
+  EVstone rstone = EValloc_stone(cm);
+  EVassoc_terminal_action(cm, rstone, alloc_data_list, cmd_handler, NULL);
 
-    // sending usage
-    usage_info data;
-    char *str = attr_list_to_string(CMget_contact_list(cm));
-    data.id = new char[strlen(str)+6];
-    sprintf(data.id, "%d:%s", rstone, str);
-    data.cpu_usage = 100;
-    data.network_in_usage = 1000;
-    data.network_out_usage = 1000;
+  // sending allocation
+  EVsource source = EVcreate_submit_handle(cm, sstone, usage_data_list);
+  lp = new LProcess("client", atoi(argv[2]), "eth0");
+  // lp->setHardCPUShares(100);
+  // lp->setNetworkOutBW(1000);
+
+  // sending usage
+  usage_info data;
+  char *str = attr_list_to_string(CMget_contact_list(cm));
+  data.id = new char[strlen(str)+6];
+  sprintf(data.id, "%d:%s", rstone, str);
+
+  // sending usage
+  while(true) {
+    CMsleep(cm, USAGE_CHECK_PERIOD);
+    data.cpu_usage = lp->getCPUCumUsage();
+    data.network_out_usage = lp->getNetworkOutCumUsage();
+    printf("%2f\t%2f\n", data.cpu_usage, data.network_out_usage);
     EVsubmit(source, &data, NULL);
+  }
 
-    // sending usage
-    while(true) {
-        CMsleep(cm, TRIGGER_PERIOD);
-        data.cpu_usage = lp->getCPUUsage();
-        data.network_in_usage = lp->getNetworkInUsage();
-        data.network_out_usage = lp->getNetworkOutUsage();
-        // printf("%2f\t%2f\t%2f\n",
-        //    data.cpu_usage, data.network_in_usage, data.network_out_usage);
-        printf("%2f\n", data.network_out_usage);
-        EVsubmit(source, &data, NULL);
-    }
-
-    return 0;
+  return 0;
 }
--- node/master.cpp
+++ node/master.cpp
@@ -1,88 +1,91 @@
 #include "dadvisor.h"
 
-static CManager cm;
-static map<string, alloc_info> cinfo;
-static map<string, EVsource> clients;
-static int count = 0;
+bool cpu_flag;
+bool network_flag;
+map<string, alloc_info> cinfo;
+map<string, EVsource> clients;
 
-static int
-alloc_handler(CManager cm, void *vevent, void *client_data, attr_list attrs)
-{
-    usage_info_ptr event = (usage_info_ptr)vevent;
-    // printf("%s => %f, %f, %f\n",
-    //     event->id, event->cpu_usage, event->network_in_usage, event->network_out_usage);
-    printf("%f\t", event->network_out_usage);
+static int alloc_handler(CManager cm, void *vevent, void *client_data,
+    attr_list attrs) {
+  usage_info_ptr event = (usage_info_ptr)vevent;
+  map<string, alloc_info>::iterator iter = cinfo.find(event->id);
 
-    if(cinfo.count(event->id) > 0)
-    {
-        if(count == 5) {
-            count = 0;
+  if(iter != cinfo.end()) {
+    // we know the client and the bridge exists
+    alloc_info new_alloc;
+    new_alloc.id = "master";
 
-            // we know the client and the bridge exists
-            alloc_info client = cinfo[event->id];
-            alloc_info new_alloc;
-            new_alloc.id = "master";
-
-            // new_alloc.cpu_alloc = -1;
-            // if(client.cpu_alloc*SAFETY_FACTOR < event->cpu_usage) {
-            //     new_alloc.cpu_alloc = client.cpu_alloc*(1+INCREMENT_FACTOR);
-            // }
-
-            // new_alloc.network_in_alloc = -1;
-            // if(client.network_in_alloc*SAFETY_FACTOR < event->network_in_usage) {
-            //     new_alloc.network_in_alloc = client.network_in_alloc*(1+INCREMENT_FACTOR);
-            // }
-
-            new_alloc.network_out_alloc = -1;
-            if(client.network_out_alloc*SAFETY_FACTOR < event->network_out_usage) {
-                new_alloc.network_out_alloc = client.network_out_alloc*(1+INCREMENT_FACTOR);
-                cinfo[event->id] = new_alloc;
-            }
-
-            EVsubmit(clients[event->id], &new_alloc, NULL);
-        }
-    } else
-    {
-        char string_list[2048];
-        attr_list contact_list;
-        EVstone remote_stone;
-
-        if(sscanf(event->id, "%d:%s", &remote_stone, &string_list[0]) != 2) {
-           printf("Bad arguments \"%s\"\n", event->id);
-           return -1;
-       }
-
-        // send stone
-        EVstone sstone = EValloc_stone(cm);
-        contact_list = attr_list_from_string(string_list);
-        EVassoc_bridge_action(cm, sstone, contact_list, remote_stone);
-        clients[event->id] = EVcreate_submit_handle(cm, sstone, command_list);
-
-        // new client
-        alloc_info client;
-        client.id = new char[strlen(event->id)];
-        strcpy(client.id, event->id);
-        client.cpu_alloc = event->cpu_usage;
-        client.network_in_alloc = event->network_in_usage;
-        client.network_out_alloc = event->network_in_usage;
-        cinfo[event->id] = client;
+    new_alloc.cpu_alloc = -1;
+    if(cpu_flag && (iter->second.cpu_alloc*SAFETY_FACTOR < event->cpu_usage)) {
+      new_alloc.cpu_alloc = iter->second.cpu_alloc*(1+INCREMENT_FACTOR);
+      iter->second.cpu_alloc = new_alloc.cpu_alloc;
     }
 
-    printf("%2f\n", cinfo[event->id].network_out_alloc);
-    count++;
+    new_alloc.network_out_alloc = -1;
+    if(network_flag && (iter->second.network_out_alloc*SAFETY_FACTOR < event->network_out_usage)) {
+      new_alloc.network_out_alloc = iter->second.network_out_alloc*(1+INCREMENT_FACTOR);
+      iter->second.network_out_alloc = new_alloc.network_out_alloc;
+    }
 
-    return 1;
+    EVsubmit(clients[event->id], &new_alloc, NULL);
+  } else {
+    char string_list[2048];
+    attr_list contact_list;
+    EVstone remote_stone;
+
+    if(sscanf(event->id, "%d:%s", &remote_stone, &string_list[0]) != 2) {
+      printf("Bad arguments \"%s\"\n", event->id);
+      return -1;
+    }
+
+    // send stone
+    EVstone sstone = EValloc_stone(cm);
+    contact_list = attr_list_from_string(string_list);
+    EVassoc_bridge_action(cm, sstone, contact_list, remote_stone);
+    clients[event->id] = EVcreate_submit_handle(cm, sstone, usage_data_list);
+
+    // new client
+    alloc_info client;
+    client.id = new char[strlen(event->id)];
+    strcpy(client.id, event->id);
+    client.cpu_alloc = event->cpu_usage;
+    cinfo[event->id] = client;
+  }
+
+  return 1;
 }
 
-int main(int argc, char **argv)
-{
-    cm = CManager_create();
-    CMlisten(cm);
+int main(int argc, char **argv) {
+  if(argc < 3) {
+    printf("usage: %s <cpu flag> <network flag>\n", argv[0]);
+    return -1;
+  }
 
-    EVstone rstone = EValloc_stone(cm);
-    EVassoc_terminal_action(cm, rstone, data_list, alloc_handler, NULL);
-    char *string_list = attr_list_to_string(CMget_contact_list(cm));
-    fprintf(stderr, "Contact list \"%d:%s\"\n", rstone, string_list);
-    CMrun_network(cm);
-    return 0;
+  // parsing the cpu flag
+  cpu_flag = false;
+  if(*argv[1] == '1') {
+    cpu_flag = true;
+  }
+
+  // parsing the network flag
+  network_flag = false;
+  if(*argv[2] == '1') {
+    network_flag = true;
+  }
+
+  // EVpath manager
+  CManager cm = CManager_create();
+  CMlisten(cm);
+
+  // creating date receiving stone
+  EVstone rstone = EValloc_stone(cm);
+  EVassoc_terminal_action(cm, rstone, usage_data_list, alloc_handler, NULL);
+
+  // printing master node contact list
+  char *string_list = attr_list_to_string(CMget_contact_list(cm));
+  fprintf(stderr, "Contact list \"%d:%s\"\n", rstone, string_list);
+
+  // loop
+  CMrun_network(cm);
+  return 0;
 }

e0dcd0a3889f50b0bade4520a9c270a9feb0f80c
Aman Mangal <mangalaman93@gmail.com>
2015-04-26 16:58:46 -0400

    sending app metrics

--- node/sipp.cpp
+++ node/sipp.cpp
@@ -2,12 +2,88 @@
 
 static LProcess *lp;
 
-void getAppMetrics(usage_info& data, FILE* pipe, FILE* pipe_rtt) {
-  char buffer[2000];
-  fgets(buffer, sizeof(buffer), pipe);
-  sscanf(buffer, "%d %d %d %d %d %d", &data.call_rate, &data.successful_calls,
+#define AWK_COMMAND "awk -F ';' '\
+BEGIN {\
+  msize=2;\
+  asize=6;\
+  array[0]=7;\
+  array[1]=15;\
+  array[2]=17;\
+  array[3]=49;\
+  array[4]=57;\
+  array[5]=59;\
+}\
+\
+{\
+  if(NR==1) {\
+    for(i=0;i<asize;i++) {\
+      final[i]=$array[i];\
+    }\
+  } else {\
+    for(i=0;i<msize;i++) {\
+      if(final[i]>$array[i]) {\
+        final[i]=$array[i];\
+      }\
+    }\
+\
+    for(i=msize;i<asize;i++) {\
+      if(final[i]<$array[i]) {\
+        final[i]=$array[i];\
+      }\
+    }\
+  }\
+}\
+\
+END {\
+  print NR;\
+  print \"t\";\
+  for(i=0;i<asize;i++) {\
+    print final[i];\
+    print \"t\";\
+  }\
+}\
+'"
+
+#define AWK_COMMAND_RTT "awk -F ';' '\
+{\
+  if(NR==1) {\
+    final=$2;\
+  } else {\
+    if(final<$3) {\
+      final=$3;\
+    }\
+  }\
+}\
+\
+END {\
+  print NR,final;\
+}\
+'"
+
+void getAppMetrics(usage_info& data, char* prefix, char* pid) {
+  static int last_line = 3;
+  static int last_line_rtt = 3;
+  int line;
+
+  // init for Application specific metrics
+  stringstream ss;
+  ss<<"tail -n +"<<last_line<<" "<<prefix<<"_"<<pid<<"_.csv | ";
+  ss<<AWK_COMMAND;
+  string result;
+  Utils::systemCmd(ss.str(), result);
+  sscanf(result.c_str(), "%dt%ft%dt%dt%dt%dt%d", &line, &data.call_rate, &data.successful_calls,
     &data.failed_calls, &data.retransmissions, &data.watchdog_major,
     &data.watchdog_minor);
+  last_line += line;
+
+  // rtt file
+  ss.str("");
+  result.clear();
+  ss<<"tail -n +"<<last_line_rtt<<" "<<prefix<<"_"<<pid<<"_rtt.csv | ";
+  ss<<AWK_COMMAND_RTT;
+  Utils::systemCmd(ss.str(), result);
+  sscanf(result.c_str(), "%d %f", &line, &data.max_response_time);
+  last_line_rtt += line;
 }
 
 static int cmd_handler(CManager cm, void *vevent, void *client_data,
@@ -65,21 +141,6 @@ int main(int argc, char **argv) {
   data.id = new char[strlen(str)+6];
   sprintf(data.id, "%d:%s", rstone, str);
 
-  // init for Application specific metrics
-  stringstream ss;
-  ss<<"tail -f -n 0 "<<argv[5]<<"_"<<argv[2]<<"_.csv | ";
-  ss<<"awk -F ';' '{print $7,$15,$17,$49,$57,$59}' ";
-  string cmd1 = ss.str();
-  FILE* pipe{ popen(cmd1.c_str(), "r")};
-  assert(pipe);
-
-  // rtt file
-  ss.str("");
-  ss<<"tail -f -n 0 "<<argv[5]<<"_"<<argv[2]<<"_rtt.csv";
-  string cmd2 = ss.str();
-  FILE* pipe_rtt{ popen(cmd2.c_str(), "r")};
-  assert(pipe_rtt);
-
   // sending usage
   struct timeval cpu_time;
   unsigned long cpu_usage = lp->getCPUCumUsage();
@@ -107,14 +168,12 @@ int main(int argc, char **argv) {
     data.network_out_usage = (net_usage-old_usage)/elapsed_time;
 
     // getting other application specific matrics
-    getAppMetrics(data, pipe, pipe_rtt);
+    getAppMetrics(data, argv[5], argv[2]);
 
-    printf("Current usage: cpu-%2f\tnet-%2f\n",
-      data.cpu_usage, data.network_out_usage);
+    printf("Current usage: callrate-%2f\tcpu-%2f\tnet-%2f\n",
+      data.call_rate, data.cpu_usage, data.network_out_usage);
     EVsubmit(source, &data, NULL);
   }
 
-  pclose(pipe);
-  pclose(pipe_rtt);
   return 0;
 }
